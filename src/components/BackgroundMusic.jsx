8f70e025ee5d65bd8971e688bde4e893cf3e77f1
Tushar-sysmain <tushar.r2080@gmail.com>
2025-08-10 20:53:15 +0530

    Update BackgroundMusic.jsx

--- src/components/BackgroundMusic.jsx
+++ src/components/BackgroundMusic.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import BackgroundMusic from "./components/BackgroundMusic";
+import MainAppUI from "./components/MainAppUI"; // if you have one
+
+function App() {
+  return (
+    <>
+      {/* This will run the background music logic */}
+      <BackgroundMusic src="public/music.mp3" />
+
+      {/* The rest of your app */}
+      <MainAppUI />
+    </>
+  );
+}
+
+export default App;
 
